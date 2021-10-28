4d22d775e8366bb7b7865bfa80394c788b6e7638
mvr05 <73706182+mvr05@users.noreply.github.com>
2021-10-28 19:22:32 +0500

    Add files via upload

---
+++ js/check.js
@@ -0,0 +1,20 @@
+/*function Ajax() {
+$.ajax({
+    url: 'http://kuldev.hostronavt.ru/?sign.check=',
+    type: 'GET',
+    data: {
+    "sign.check": $("input[type='author\\[\\]']:checked").serialize(),
+    //party
+    categoryid: $("input[name='category\\[\\]']:checked").serialize(),
+    //password
+    text: $("#text").val()
+}
+}).done(function(result) {
+$("#output").html(result);
+$("#output").css({
+"display": "block"
+});
+});
+}*/
+/*/?sign.check={"fullname":"Куликов%20Денис%20Ильич","party":"ПИЭ-18-2","password":"root"}*/
+
